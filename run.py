5505fb9551b4da154ab9a6935c1f055af716dd93
trippy22 <33383800+trippy22@users.noreply.github.com>
2020-12-11 01:10:42 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from websocket import config, wsserver
 
 # Runs server
-print(f'Starting server on port %s. Waiting for connections... {config.socket_port}')
+print(f'Starting server on port {config.socket_port}. Waiting for connections... ')
 wsserver.Server().run()
